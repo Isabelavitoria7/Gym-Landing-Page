69f5c9c145b285072e94c54c32eb3eb6ef141fb1
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2024-06-27 22:51:32 -0300

    Team session styling and carousel application

--- assets/js/pages/equipe/team.js
+++ assets/js/pages/equipe/team.js
@@ -1,1 +1,23 @@
-console.log('hejijd')
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.personal-carousel');
+    const chevrons = document.querySelectorAll('.chevron');
+    let currentIndex = 0;
+    const totalItems = document.querySelectorAll('.personal').length;
+
+    function updateCarrousel() {
+        carousel.style.transform = `translateX(${-(currentIndex * 100)}%)`;
+    }
+
+    chevrons.forEach(chevron => {
+        chevron.addEventListener('click', function() {
+            if (chevron.classList.contains('chevron-left')) {
+                currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+            } else {
+                currentIndex = (currentIndex < totalItems - 1) ? currentIndex + 1 : 0;
+            }
+            updateCarrousel();
+        });
+    });
+
+    updateCarrousel();
+});
