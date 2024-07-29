710dde2854abbaf5204946a83c80cf0779b69de9
Isabela Vitoria <isabelavitoriabarroso2004@gmail.com>
2024-07-29 16:35:56 -0300

    home page responsiveness

---
+++ assets/js/home.js
@@ -0,0 +1,32 @@
+
+const menuHamburguer = document.querySelector(".hamburguer");
+const showLinks = document.querySelector(".links");
+
+menuHamburguer.addEventListener('click', function(){
+    if(menuHamburguer.classList.contains('active')){
+        showLinks.style.display = 'flex';
+    }else{
+        showLinks.style.display = 'none';
+    }
+})
+
+function adjustMenuDisplay() {
+    if (window.innerWidth > 800) {
+        showLinks.style.display = 'flex';
+    } else if (!menuHamburguer.classList.contains('active')) {
+        showLinks.style.display = 'none';
+    }
+}
+
+window.addEventListener('resize', adjustMenuDisplay);
+
+document.addEventListener('scroll', function() {
+    const setaDivisoria = document.querySelector('.container-divisoria');
+    if (window.scrollY > 0) {
+        setaDivisoria.classList.add('hidden');
+    } else {
+        setaDivisoria.classList.remove('hidden');
+    }
+});
+
+
